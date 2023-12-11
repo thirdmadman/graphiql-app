48b70778069fa8dadd52817180487f4a2a57ace3
thirdmadman <thirdmadman@gmail.com>
2023-12-11 22:18:20 +0100

    feat: add redux

---
+++ src/lib/redux/store.ts
@@ -0,0 +1,23 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import detailsReducer, { detailsSlice } from './features/todos/detailsSlice';
+
+const reducers = {
+  [detailsSlice.name]: detailsReducer,
+};
+
+const combinedReducer = combineReducers<typeof reducers>(reducers);
+
+const store = configureStore({
+  reducer: combinedReducer,
+});
+
+export function setupStore(preloadedState?: RootState) {
+  return configureStore({
+    reducer: combinedReducer,
+    preloadedState,
+  });
+}
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];

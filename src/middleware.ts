7c78ae7db5f7208661fade3a975e5e280c60bb6b
thirdmadman <thirdmadman@gmail.com>
2023-12-12 00:26:54 +0100

    feat: add protected page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 
 //Protected routes
 export const config = {
-  matcher: ['/app/:path*'],
+  matcher: ['/protected/:path*'],
 };
 
 export async function middleware(request: NextRequest) {

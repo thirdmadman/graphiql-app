77a1670a06043f68410074c0dc4d117928110bf0
thirdmadman <thirdmadman@gmail.com>
2023-12-12 00:20:38 +0100

    chore: correct imports/exports

--- src/lib/redux/StoreProvider.tsx
+++ src/lib/redux/StoreProvider.tsx
@@ -4,11 +4,7 @@ import { useRef } from 'react';
 import { Provider } from 'react-redux';
 import { setupStore, AppStore } from './store';
 
-export default function StoreProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export function StoreProvider({ children }: { children: React.ReactNode }) {
   const storeRef = useRef<AppStore>();
   if (!storeRef.current) {
     storeRef.current = setupStore();

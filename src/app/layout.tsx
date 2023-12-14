f750eaf06f59831b240ef6159e11c41c92265849
thirdmadman <thirdmadman@gmail.com>
2023-12-14 17:52:24 +0100

    feat: add next ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { UIProvider } from '@/lib/nextui/UIProvider';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="light">
+      <body className={inter.className}>
+        <UIProvider>{children}</UIProvider>
+      </body>
     </html>
   );
 }

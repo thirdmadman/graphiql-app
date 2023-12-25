8738253ad74a4b2a4f6a00555c2cf243c2025ee7
iamnkt <sg23mj@gmail.com>
2023-12-25 03:45:13 +0300

    fix: fix styles, content text

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -7,17 +7,16 @@ export default async function Welcome() {
 
   return (
     <main className="container flex flex-col items-center mx-auto">
-      <section className="py-8 flex flex-col-reverse justify-center md:py-10 lg:flex-row lg:justify-between lg:py-12">
-        <div className="flex items-center justify-center p-6 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-          <Image
-            priority
-            src={'/images/svg/welcome_logo.svg'}
-            alt="Logo"
-            width={450}
-            height={450}
-          />
-        </div>
-        <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+      <section className="py-8 flex flex-col-reverse justify-center md:pb-12 lg:flex-row lg:justify-between lg:pb-16">
+        <Image
+          priority
+          src={'/images/svg/welcome_logo.svg'}
+          alt="Logo"
+          width={450}
+          height={450}
+          className="py-4 mx-auto lg:py-0"
+        />
+        <div className="flex flex-col justify-center text-center rounded-sm lg:pl-6 lg:max-w-md xl:max-w-lg lg:text-left">
           <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-6 lg:justify-start">
             {user ? (
               <Link
@@ -55,18 +54,18 @@ export default async function Welcome() {
             to the GraphiQL
             <br></br>
           </h3>
-          <p className="text-lg md:px-24 md:text-justify lg:px-0">
+          <p className="text-lg px-4 md:px-24 md:py-0 md:text-justify lg:p-0">
             Our app easily allows you to make requests to the variety of open
             GraphQL APIs
           </p>
         </div>
       </section>
-      <section className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 md:py-10 lg:px-8 lg:py-12">
+      <section className="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-md md:px-24 md:py-12 lg:px-8 lg:py-16">
         <h4 className="mb-8 text-4xl font-bold leadi text-center">
-          What do we have to offer?
+          Why do people prefer our app?
         </h4>
         <ul className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-          <li className="flex items-center space-x-2">
+          <li className="flex px-1 items-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
@@ -75,9 +74,9 @@ export default async function Welcome() {
               <path d="M426.072,86.928A238.75,238.75,0,0,0,88.428,424.572,238.75,238.75,0,0,0,426.072,86.928ZM257.25,462.5c-114,0-206.75-92.748-206.75-206.75S143.248,49,257.25,49,464,141.748,464,255.75,371.252,462.5,257.25,462.5Z"></path>
               <polygon points="221.27 305.808 147.857 232.396 125.23 255.023 221.27 351.063 388.77 183.564 366.142 160.937 221.27 305.808"></polygon>
             </svg>
-            <span>Extremly fast performance</span>
+            <span>Extra fast performance</span>
           </li>
-          <li className="flex items-center space-x-2">
+          <li className="flex px-1 items-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
@@ -88,7 +87,7 @@ export default async function Welcome() {
             </svg>
             <span>User-friendly interface</span>
           </li>
-          <li className="flex items-center space-x-2">
+          <li className="flex px-1 items-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
@@ -99,7 +98,7 @@ export default async function Welcome() {
             </svg>
             <span>Great user experience</span>
           </li>
-          <li className="flex items-center space-x-2">
+          <li className="flex px-1 items-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
@@ -110,7 +109,7 @@ export default async function Welcome() {
             </svg>
             <span>Plenty of supported APIs</span>
           </li>
-          <li className="flex items-center space-x-2">
+          <li className="flex px-1 items-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
@@ -119,9 +118,9 @@ export default async function Welcome() {
               <path d="M426.072,86.928A238.75,238.75,0,0,0,88.428,424.572,238.75,238.75,0,0,0,426.072,86.928ZM257.25,462.5c-114,0-206.75-92.748-206.75-206.75S143.248,49,257.25,49,464,141.748,464,255.75,371.252,462.5,257.25,462.5Z"></path>
               <polygon points="221.27 305.808 147.857 232.396 125.23 255.023 221.27 351.063 388.77 183.564 366.142 160.937 221.27 305.808"></polygon>
             </svg>
-            <span>API documentation loading</span>
+            <span>API docs loading</span>
           </li>
-          <li className="flex items-center space-x-2">
+          <li className="flex px-1 items-center space-x-2">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
@@ -134,39 +133,41 @@ export default async function Welcome() {
           </li>
         </ul>
       </section>
-      <section className="flex flex-col-reverse justify-center px-4 py-8 mx-auto sm:max-w-xl md:max-w-full md:py-10 lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12 lg:flex-row lg:justify-between">
-        <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+      <section className="flex flex-col-reverse justify-center px-4 py-8 mx-auto sm:max-w-xl md:max-w-full md:py-12 lg:max-w-screen-md md:px-24 lg:px-8 lg:py-16 lg:flex-row lg:justify-between">
+        <div className="flex flex-col justify-center text-center rounded-sm lg:pr-6 lg:max-w-md xl:max-w-lg lg:text-left">
           <h4 className="mb-8 text-4xl font-bold leadi text-center">
-            Why did we build this?
+            Our motivation
           </h4>
-          <p className="text-justify">
+          <p className="p-1 text-justify">
             Our team is really interested in the idea of writing a project using
-            the latest web apps development technologies. Thanks to the RS
-            school we got acquainted with such a wonderful framework as Next.js.
-            Therefore we decided to apply the Next.js App Router paradigm for
-            building our application using React&apos;s latest features, such as
-            server-side &#60;Suspense&#62; component and others. Also, while
-            working on this project, we really improved our teamwork and coding
-            skills. And overall, it was a very entertaining and useful
-            experience for us.
+            the latest web app development technologies. Thanks to the RS-school
+            React course, we got acquainted with such a wonderful framework as
+            Next.js.<br></br>
+            <br></br> Therefore, we decided to apply the Next.js App Router
+            paradigm for building our application with API routes and
+            middleware. We opted to design our application with React&apos;s
+            latest features, such as server-side components, Suspense, and
+            others.<br></br>
+            <br></br> While working on this project, we significantly enhanced
+            our teamwork and coding skills. And overall, it was a very
+            entertaining and useful experience for us.
           </p>
         </div>
-        <div className="flex items-center justify-center p-6 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-          <Image
-            priority
-            src={'/images/svg/welcome_project.svg'}
-            alt="Project"
-            width={265}
-            height={265}
-          />
-        </div>
+        <Image
+          priority
+          src={'/images/svg/welcome_project.svg'}
+          alt="Project"
+          width={250}
+          height={250}
+          className="py-4 mx-auto lg:py-0"
+        />
       </section>
-      <section className="py-8 flex flex-col items-center justify-center px-4 mx-auto sm:p-10 lg:py-12">
-        <h4 className="text-4xl mb-4 font-bold leadi text-center sm:mb-0">
+      <section className="py-8 flex flex-col items-center justify-center px-4 mx-auto md:py-12 lg:py-16">
+        <h4 className="text-4xl mb-4 font-bold leadi text-center sm:mb-8">
           Our team
         </h4>
         <div className="flex justify-between items-start">
-          <div className="flex flex-col justify-center m-4 text-center sm:m-8">
+          <div className="flex flex-col justify-center mx-4 text-center sm:mx-8">
             <Image
               alt="Person"
               className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full"
@@ -177,7 +178,7 @@ export default async function Welcome() {
             <p className="text-xl font-semibold leadi">thirdmadman</p>
             <p className="text-sm">Team lead</p>
           </div>
-          <div className="flex flex-col m-4 justify-center text-center sm:m-8">
+          <div className="flex flex-col mx-4 justify-center text-center sm:mx-8">
             <Image
               alt="Person"
               className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full"
@@ -189,7 +190,7 @@ export default async function Welcome() {
             <p className="text-xl font-semibold leadi">nvalkovich</p>
             <p className="text-sm">Web developer</p>
           </div>
-          <div className="flex flex-col m-4 justify-center text-center sm:m-8">
+          <div className="flex flex-col mx-4 justify-center text-center sm:mx-8">
             <Image
               alt="Person"
               className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full"

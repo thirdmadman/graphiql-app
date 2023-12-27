e57776a3109ad7896bd9629eea9f670ee75a8170
iamnkt <sg23mj@gmail.com>
2023-12-27 22:07:43 +0300

    feat: add yup schema for validation

--- src/lib/yup/formSchema.ts
+++ src/lib/yup/formSchema.ts
@@ -1,16 +1,29 @@
 import * as yup from 'yup';
+import { ref } from 'yup';
 
 export const formSchema = yup.object().shape({
   email: yup
     .string()
-    .email('Invalid email address')
-    .required('Email is a required field'),
+    .email('Please enter a valid email address.')
+    .required('Required field'),
   password: yup
     .string()
-    .min(6, 'Password must be 6 or more characters')
-    .matches(/[a-z]/, 'Password must contain at least 1 lowercase letter')
-    .matches(/[A-Z]/, 'Password must contain at least 1 uppercase letter')
-    .matches(/[0-9]/, 'Password must contain at least 1 number')
-    .matches(/\W|_/, 'Password must contain at least 1 special character')
-    .required('Password is required field'),
+    .matches(/\W|_/, 'Must contain at least one special character')
+    .matches(/[A-Z]/, 'Must contain at least one uppercase letter')
+    .matches(/[a-z]/, 'Must contain at least one lowercase letter')
+    .matches(/[0-9]/, 'Must contain at least one number')
+    .min(8, 'Must be 8 or more characters')
+    .required('Required field'),
+  confirmpassword: yup
+    .string()
+    .oneOf([ref('password')], 'Passwords do not match')
+    .matches(/\W|_/, 'Must contain at least one special character')
+    .matches(/[A-Z]/, 'Must contain at least one uppercase letter')
+    .matches(/[a-z]/, 'Must contain at least one lowercase letter')
+    .matches(/[0-9]/, 'Must contain at least one number')
+    .min(8, 'Must be 8 or more characters')
+    .required('Required field'),
+  terms: yup
+    .boolean()
+    .test('You should accept terms and conditions', (terms) => !!terms),
 });

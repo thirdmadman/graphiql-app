aa9b916a1f1431269f345335af6680103821727c
thirdmadman <thirdmadman@gmail.com>
2023-12-29 01:09:34 +0100

    refactor: IntrospectionEnumTypeComponent

--- src/app/components/introspection/enum/IntrospectionEnumTypeComponent.tsx
+++ src/app/components/introspection/enum/IntrospectionEnumTypeComponent.tsx
@@ -9,9 +9,10 @@ import { useState } from 'react';
 import { IntrospectionEnumValueComponent } from './IntrospectionEnumValueCopmponent';
 import { DescriptionFieldComponent } from '../shared/DescriptionFieldComponent';
 
-interface IEnumTypeComponentProps {
+interface IIntrospectionEnumTypeComponentProps {
   type: IntrospectionEnumType | undefined | null;
   schema: IntrospectionSchema;
+  isOpenedSet?: boolean;
 }
 
 // IntrospectionEnumType {
@@ -21,8 +22,11 @@ interface IEnumTypeComponentProps {
 //   enumValues: Array<IntrospectionEnumValue>;
 // }
 
-export function EnumTypeComponent({ type }: IEnumTypeComponentProps) {
-  const [isOpened, setIsOpened] = useState(false);
+export function IntrospectionEnumTypeComponent({
+  type,
+  isOpenedSet = false,
+}: IIntrospectionEnumTypeComponentProps) {
+  const [isOpened, setIsOpened] = useState(isOpenedSet);
 
   if (!type) {
     return;
@@ -34,14 +38,12 @@ export function EnumTypeComponent({ type }: IEnumTypeComponentProps) {
         className="mb-1 cursor-pointer"
         onClick={() => setIsOpened(!isOpened)}
       >
-        <b>{type.name}</b>
+        type: {type.kind}: <i>{type.name}</i>
       </div>
       <div className={isOpened ? '' : 'hidden'}>
         <DescriptionFieldComponent description={type.description} />
         <div className="pl-2 mb-2 border-l-4 border-indigo-500">
-          <div className="mb-1">
-            <b>Enum values:</b>
-          </div>
+          <div className="mb-1">ENUM values:</div>
           {type.enumValues.map((value) => (
             <IntrospectionEnumValueComponent
               value={value}

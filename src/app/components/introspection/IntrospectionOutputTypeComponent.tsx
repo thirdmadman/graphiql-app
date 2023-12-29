dc6750ba4c94e4ed3aa0716168235b6ad7fda728
thirdmadman <thirdmadman@gmail.com>
2023-12-29 01:07:35 +0100

    feat: update render

--- src/app/components/introspection/IntrospectionOutputTypeComponent.tsx
+++ src/app/components/introspection/IntrospectionOutputTypeComponent.tsx
@@ -42,16 +42,21 @@ export function IntrospectionOutputTypeComponent({
   }
 
   return (
-    <div className="pl-2 mb-2 border-l-4 border-indigo-500">
-      <div
-        className="mb-1 cursor-pointer"
-        onClick={() => setIsOpened(!isOpened)}
-      >
-        type: {type.kind}: {type.name}
-      </div>
+    <>
+      {!isOpened && (
+        <div className="pl-2 mb-2 border-l-4 border-indigo-500">
+          <div
+            className="mb-1 cursor-pointer"
+            onClick={() => setIsOpened(!isOpened)}
+          >
+            type: {type.kind}: <i>{type.name}</i>
+          </div>
+        </div>
+      )}
+
       {isOpened && (
         <GetFieldWrapper kind={type.kind} name={type.name} schema={schema} />
       )}
-    </div>
+    </>
   );
 }

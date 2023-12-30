b450fb41903dbbcf89805bf1efc40e1141aedb19
thirdmadman <thirdmadman@gmail.com>
2023-12-30 20:42:53 +0100

    feat: add error message

--- src/app/components/DocumentationComponent.tsx
+++ src/app/components/DocumentationComponent.tsx
@@ -13,13 +13,22 @@ export async function DocumentationComponent({
 
   const schemaResponse = await getGqlIntrospection(
     'https://spacex-production.up.railway.app',
+
     headers?.resp ?? {}
   );
 
   const { schema, error } = schemaResponse;
 
   if (!schema || error) {
-    return;
+    return (
+      <div
+        className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+        role="alert"
+      >
+        <span className="font-medium">We are sorry,</span> but documentation is
+        not available at the moment
+      </div>
+    );
   }
 
   return <IntrospectionRoot schema={schema.__schema} />;

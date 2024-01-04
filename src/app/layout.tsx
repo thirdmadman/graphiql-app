450163602165547a3b7b3b99edba66ef2b19bf4e
thirdmadman <thirdmadman@gmail.com>
2024-01-04 11:40:39 +0100

    feat: add favicons to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="light">
+      <head>
+        <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicons/favicon-96x96.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/favicons/apple-icon-60x60.png"
+          type="image/png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-icon-180x180.png"
+          type="image/png"
+        />
+        <link rel="manifest" href="/favicons/manifest.json" />
+      </head>
       <body className={inter.className}>
         <UIProvider>
           {children}

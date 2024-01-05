79e10d074910bf3b56d2661cda2462eb0b0d41c5
thirdmadman <thirdmadman@gmail.com>
2024-01-05 23:43:31 +0100

    test: add test cases for FoldableBlockComponent

--- src/app/components/introspection/shared/FoldableBlockComponent.tsx
+++ src/app/components/introspection/shared/FoldableBlockComponent.tsx
@@ -21,18 +21,24 @@ export function FoldableBlockComponent({
 
   return (
     <div className="pl-2 mb-2 border-l-4 border-indigo-800 relative">
-      <div className="absolute -left-2 top-1.5 w-3 h-3 bg-black text-white leading-3 text-center font-mono dark:bg-white dark:text-black">
+      <div
+        data-testid="icon"
+        className="absolute -left-2 top-1.5 w-3 h-3 bg-black text-white leading-3 text-center font-mono dark:bg-white dark:text-black"
+      >
         {isOpened ? '-' : '+'}
       </div>
       <div
         className="mb-2 cursor-pointer"
         onClick={() => setIsOpened(!isOpened)}
+        data-testid="title"
       >
         {title}
       </div>
 
       {inside !== undefined && inside !== null && (
-        <div className={isOpened ? '' : 'hidden'}>{inside}</div>
+        <div className={isOpened ? '' : 'hidden'} data-testid="inside">
+          {inside}
+        </div>
       )}
     </div>
   );

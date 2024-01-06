5a40fbefad9d621d2ef68e64a586cab407590518
thirdmadman <thirdmadman@gmail.com>
2024-01-06 02:40:56 +0100

    test: add cases for AllFieldsTypeFieldComponent

--- src/app/components/introspection/AllFieldsTypeFieldComponent.tsx
+++ src/app/components/introspection/AllFieldsTypeFieldComponent.tsx
@@ -24,12 +24,16 @@ export function AllFieldsTypeFieldComponent({
 
   return (
     <div className="pl-2 mb-2 border-l-4 border-indigo-500 relative">
-      <div className="absolute -left-2 top-1.5 w-3 h-3 bg-black text-white leading-3 text-center font-mono dark:bg-white dark:text-black">
+      <div
+        data-testid="AllFieldsTypeIcon"
+        className="absolute -left-2 top-1.5 w-3 h-3 bg-black text-white leading-3 text-center font-mono dark:bg-white dark:text-black"
+      >
         {isOpened ? '-' : '+'}
       </div>
       <div
         className="mb-2 cursor-pointer"
         onClick={() => setIsOpened(!isOpened)}
+        data-testid="AllFieldsTypeTitle"
       >
         <b>All types:</b>
       </div>

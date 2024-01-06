ac8f4172046b524d853f33511f2caaf5b11eb060
iamnkt <sg23mj@gmail.com>
2024-01-06 15:00:29 +0300

    fix: add providers

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,8 +1,20 @@
 import { getUser } from '@/lib/firebase/getUser';
 import { WelcomeContent } from '../components/WelcomePageContent';
+import { LanguageSelector } from '@/app/components/LanguageSelector';
+import { StoreProvider } from '@/lib/redux/StoreProvider';
+import { LocaleProvider } from '@/locales/localeProvider';
 
 export default async function Welcome() {
   const userId = await getUser();
 
-  return <WelcomeContent userId={userId} />;
+  return (
+    <StoreProvider>
+      <LocaleProvider>
+        <div className="flex min-h-screen flex-col items-center">
+          <LanguageSelector />
+          <WelcomeContent userId={userId} />
+        </div>
+      </LocaleProvider>
+    </StoreProvider>
+  );
 }

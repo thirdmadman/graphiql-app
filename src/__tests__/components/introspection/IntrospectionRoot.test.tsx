b0ab919709a518a4de19fc88c045529f963bc79b
thirdmadman <thirdmadman@gmail.com>
2024-01-06 14:30:30 +0100

    test: add basic test case for DeprecationReasonComponent

---
+++ src/__tests__/components/introspection/IntrospectionRoot.test.tsx
@@ -0,0 +1,16 @@
+import { mockGqlSchemaIntrospection } from '@/__tests__/__mocks__/mockGqlSchemaIntrospection';
+import { IntrospectionRoot } from '@/app/components/introspection/IntrospectionRoot';
+import { IIntrospectionSchema } from '@/lib/utils/gql/introspectionImportedTypes';
+import { render } from '@testing-library/react';
+
+describe('DeprecationReasonComponent', () => {
+  it('should render component and not fail', () => {
+    const { container } = render(
+      <IntrospectionRoot
+        schema={mockGqlSchemaIntrospection as unknown as IIntrospectionSchema}
+      />
+    );
+
+    expect(container).not.toBeNull();
+  });
+});

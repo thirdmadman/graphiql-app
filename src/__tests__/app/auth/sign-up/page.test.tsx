2cb95c2fd170bc157d1923e2fbf3493b1bc4e059
thirdmadman <thirdmadman@gmail.com>
2024-01-11 13:54:36 +0100

    refactor: correct cleanup code

--- src/__tests__/app/auth/sign-up/page.test.tsx
+++ src/__tests__/app/auth/sign-up/page.test.tsx
@@ -22,53 +22,49 @@ vi.mock('@/app/components/shared/header/Header', () => ({
 }));
 
 describe('Sign-up page', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+    render(SignUp());
+  });
+
   it('should render without failing', () => {
     const { container } = render(SignUp());
     expect(container.firstElementChild).not.toBeNull();
   });
 
   it('should contain form title', () => {
-    render(SignUp());
     expect(screen.getByText('Create an account')).not.toBeNull();
   });
 
   it('should contain login link', () => {
-    render(SignUp());
     expect(screen.getByText('Login here')).not.toBeNull();
   });
 
   it('should contain name field', () => {
-    render(SignUp());
     expect(screen.getByText('Name')).not.toBeNull();
   });
 
   it('should contain email adress field', () => {
-    render(SignUp());
     expect(screen.getByText('Email address')).not.toBeNull();
   });
 
   it('should contain password field', () => {
-    render(SignUp());
     expect(screen.getByText('Password')).not.toBeNull();
   });
 
   it('should contain confirm password field', () => {
-    render(SignUp());
     expect(screen.getByText('Confirm password')).not.toBeNull();
   });
 
   it('should contain terms and conditions field', () => {
-    render(SignUp());
     expect(screen.getByText('Terms and Conditions')).not.toBeNull();
   });
 
   it('should contain sign up button', () => {
-    render(SignUp());
     expect(screen.getByText('Sign up')).not.toBeNull();
   });
 
   it('should render errors on pressing submit', async () => {
-    render(SignUp());
     await userEvent.click(screen.getByText('Sign up'));
     expect(screen.getByText('Must be 2 or more characters')).not.toBeNull();
     expect(screen.getAllByText('Required field')).not.toBeNull();
@@ -78,13 +74,11 @@ describe('Sign-up page', () => {
   });
 
   it('should render errors typing name', async () => {
-    render(SignUp());
     await userEvent.type(screen.getByTestId('name'), 't');
     expect(screen.getByText('Must be 2 or more characters')).not.toBeNull();
   });
 
   it('should render errors typing email', async () => {
-    render(SignUp());
     await userEvent.type(screen.getByTestId('email'), 'test');
     expect(
       screen.getByText('Please enter a valid email address')
@@ -92,19 +86,16 @@ describe('Sign-up page', () => {
   });
 
   it('should render errors typing password', async () => {
-    render(SignUp());
     await userEvent.type(screen.getByTestId('password'), 'test');
     expect(screen.getByText('Must be 8 or more characters')).not.toBeNull();
   });
 
   it('should render errors typing password confimation', async () => {
-    render(SignUp());
     await userEvent.type(screen.getByTestId('confirm-password'), 'test');
     expect(screen.getByText('Passwords do not match')).not.toBeNull();
   });
 
   it('should render errors unclicking terms and conditions field', async () => {
-    render(SignUp());
     await userEvent.dblClick(screen.getByTestId('terms'));
     expect(
       screen.getByText('You should accept terms and conditions')
@@ -112,25 +103,21 @@ describe('Sign-up page', () => {
   });
 
   it('should render weak password message', async () => {
-    render(SignUp());
     await userEvent.type(screen.getByTestId('password'), 'test');
     expect(screen.getByText('weak')).not.toBeNull();
   });
 
   it('should render okay password message', async () => {
-    render(SignUp());
     await userEvent.type(screen.getByTestId('password'), 'test123!');
     expect(screen.getByText('okay')).not.toBeNull();
   });
 
   it('should render good password message', async () => {
-    render(SignUp());
     await userEvent.type(screen.getByTestId('password'), 'test123!te');
     expect(screen.getByText('good')).not.toBeNull();
   });
 
   it('should render strong password message', async () => {
-    render(SignUp());
     await userEvent.type(screen.getByTestId('password'), 'test123!test');
     expect(screen.getByText('strong')).not.toBeNull();
   });

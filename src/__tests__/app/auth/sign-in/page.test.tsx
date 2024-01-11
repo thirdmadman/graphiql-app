22d0584913ab0c55c907721f8c01fbec21649585
thirdmadman <thirdmadman@gmail.com>
2024-01-11 13:51:27 +0100

    refactor: correct cleanup code

--- src/__tests__/app/auth/sign-in/page.test.tsx
+++ src/__tests__/app/auth/sign-in/page.test.tsx
@@ -4,10 +4,6 @@ import userEvent from '@testing-library/user-event';
 
 describe('SignIn page', () => {
   beforeAll(() => {
-    beforeEach(() => {
-      vi.clearAllMocks();
-    });
-
     vi.mock('next/navigation', async () => {
       const actual = await vi.importActual('next/navigation');
       return {
@@ -28,25 +24,27 @@ describe('SignIn page', () => {
     }));
   });
 
+  beforeEach(() => {
+    vi.clearAllMocks();
+    render(SignIn());
+  });
+
   it('should render without failing', () => {
     const { container } = render(SignIn());
     expect(container.firstElementChild).not.toBeNull();
   });
 
   it('should contain form title', () => {
-    render(SignIn());
     expect(screen.getByText('Sign in')).not.toBeNull();
   });
 
   it('should render errors on pressing submit', async () => {
-    render(SignIn());
     await userEvent.click(screen.getByText('Sign in'));
     expect(screen.getByText('Must be 8 or more characters')).not.toBeNull();
     expect(screen.getAllByText('Required field')).not.toBeNull();
   });
 
   it('should render errors typing email', async () => {
-    render(SignIn());
     await userEvent.type(screen.getByTestId('email'), 'test');
     expect(
       screen.getByText('Please enter a valid email address')
@@ -54,13 +52,11 @@ describe('SignIn page', () => {
   });
 
   it('should render errors typing password', async () => {
-    render(SignIn());
     await userEvent.type(screen.getByTestId('password'), 'test');
     expect(screen.getByText('Must be 8 or more characters')).not.toBeNull();
   });
 
   it('should toggle password type on eye icon click', async () => {
-    render(SignIn());
     await userEvent.type(screen.getByTestId('password'), 'test');
     await userEvent.click(screen.getByTestId('eye-svg'));
     expect(screen.getByTestId('password').getAttribute('type')).toBe('text');

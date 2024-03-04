88d170042f7649cdd68040083dcf6d1679edd27e
thirdmadman <thirdmadman@gmail.com>
2024-03-04 01:42:07 +0100

    feat: add ThemeSwitcher

---
+++ src/app/components/shared/header/ThemeSwitcher.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+export function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const isDark = theme === 'dark';
+
+  return (
+    <div>
+      <button onClick={() => setTheme(isDark ? 'light' : 'dark')}>
+        {isDark ? 'O' : 'C'}
+      </button>
+    </div>
+  );
+}

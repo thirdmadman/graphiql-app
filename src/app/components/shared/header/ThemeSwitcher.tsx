07752943b99ff6d0b329cdd634c4f2e125cbdb79
thirdmadman <thirdmadman@gmail.com>
2024-03-04 13:26:30 +0100

    feat: add icons

--- src/app/components/shared/header/ThemeSwitcher.tsx
+++ src/app/components/shared/header/ThemeSwitcher.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import Image from 'next/image';
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
 
@@ -7,18 +7,36 @@ export function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
+  const darkModeImage = (
+    <Image
+      src="images/svg/icons/icon-dark-mode.svg"
+      width="24"
+      height="24"
+      alt="light"
+    />
+  );
+
+  const lightModeImage = (
+    <Image
+      src="/images/svg/icons/icon-light-mode.svg"
+      width="24"
+      height="24"
+      alt="light"
+    />
+  );
+
   useEffect(() => {
     setMounted(true);
   }, []);
 
-  if (!mounted) return null;
+  if (!mounted) return <div>{lightModeImage}</div>;
 
   const isDark = theme === 'dark';
 
   return (
     <div>
       <button onClick={() => setTheme(isDark ? 'light' : 'dark')}>
-        {isDark ? 'O' : 'C'}
+        {isDark ? lightModeImage : darkModeImage}
       </button>
     </div>
   );

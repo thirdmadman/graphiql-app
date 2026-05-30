4d2335f45b5ff948691c35fc7cff78904d47d6ea
thirdmadman <thirdmadman@gmail.com>
2026-05-31 01:23:00 +0200

    fix: update deps

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -23,36 +23,39 @@ export default function Home({
   return (
     <>
       <Header />
-      <main className="container flex min-h-screen flex-col items-center max-w-screen-xxl mx-auto py-24 px-2 sm:px-8 md:px-12 lg:px-24 xl:px-32 xl:py-32">
-        <EditorTitle />
-        <UrlInput
-          urlOverwrite={searchParams?.url && String(searchParams?.url)}
-        />
-        <div className="flex flex-wrap min-w-full justify-around relative">
-          <RequestWrapper />
-          <Suspense
-            key={generateSuspenseKeyBySearchParams(searchParams)}
-            fallback={<TextareaField mode={Mode.Readonly} isLoading={true} />}
-          >
-            <ResponseWrapper searchParams={searchParams} />
-          </Suspense>
-        </div>
-        <div className="py-10 w-full flex">
-          <Suspense
-            key={generateSuspenseKeyBySearchParams(searchParams, [
-              'url',
-              'headers',
-            ])}
-            fallback={
-              <div>
-                <LocaleByNameExtractor localeName="loadingText" />
-              </div>
-            }
-          >
-            <DocumentationComponent searchParams={searchParams} />
-          </Suspense>
-        </div>
-      </main>
+      <Suspense fallback={<div>Loading editor...</div>}>
+        {/*TODO: This should be fixed! */}
+        <main className="container flex min-h-screen flex-col items-center max-w-screen-xxl mx-auto py-24 px-2 sm:px-8 md:px-12 lg:px-24 xl:px-32 xl:py-32">
+          <EditorTitle />
+          <UrlInput
+            urlOverwrite={searchParams?.url && String(searchParams?.url)}
+          />
+          <div className="flex flex-wrap min-w-full justify-around relative">
+            <RequestWrapper />
+            <Suspense
+              key={generateSuspenseKeyBySearchParams(searchParams)}
+              fallback={<TextareaField mode={Mode.Readonly} isLoading={true} />}
+            >
+              <ResponseWrapper searchParams={searchParams} />
+            </Suspense>
+          </div>
+          <div className="py-10 w-full flex">
+            <Suspense
+              key={generateSuspenseKeyBySearchParams(searchParams, [
+                'url',
+                'headers',
+              ])}
+              fallback={
+                <div>
+                  <LocaleByNameExtractor localeName="loadingText" />
+                </div>
+              }
+            >
+              <DocumentationComponent searchParams={searchParams} />
+            </Suspense>
+          </div>
+        </main>
+      </Suspense>
     </>
   );
 }
--- src/lib/firebase/getUser.ts
+++ src/lib/firebase/getUser.ts
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers';
 import { adminAuth } from './firebaseAdminConfig';
 
 export async function getUser() {
-  const session = cookies().get('session')?.value;
+  const session = (await cookies()).get('session')?.value;
   if (!session || !adminAuth) {
     return null;
   }
